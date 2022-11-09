880a816029c4c46b42b3dabf550371f0c7bb6103
Aleardo Kandiah <aleardotambi6@gmail.com>
2022-11-09 13:14:40 +0000

    skill edits

--- components/Skill10.tsx
+++ components/Skill10.tsx
@@ -20,12 +20,12 @@ function Skill10({directionLeft}: Props) {
             className='rounded-full border border-gray-500 object-cover w-16 h-16 
             md:w-18 md:h-18 xl:w-22 xl:h-22 filter group-hover:grayscale transition duration-300 ease-in-out'
         />        
-        <div className='absolute opacity-0 group-hover:opacity-80 transition duration-300
+        {/* <div className='absolute opacity-0 group-hover:opacity-80 transition duration-300
          ease-in-out group-hover:bg-white h-16 w-16 md:w-18 md:h-18 xl:w-22 xl:h-22 rounded-full z-0'>
             <div className='flex items-center justify-center h-full'>
                 <p className='text-1xl font-bold text-black opacity-100'>85%</p>
             </div>
-        </div>
+        </div> */}
     </div>
   )
 }
